13a6ed285c20d79be2f42644a9b82f8ab1fff1e3
Catherine Bao <112794244+CatherineBao@users.noreply.github.com>
2022-11-29 21:51:46 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   const h1 = 'uppercase text-white-gray text-xl mb-4 mt-5 lg:mt-0'
   return (
     <div className='bg-dark-gray py-10 mt-10 sm:mt-20 px-4 sm:px-14 lg:px-36'>
-  	<div className='flex flex-col lg:flex-row items-center justify-center lg:justify-between lg:gap-x-20 leading-8  '>
+  	<div className='flex flex-col lg:flex-row items-start justify-center lg:justify-between lg:gap-x-20 leading-8  '>
   		<div className='lg:w-1/2'>
   			<h3 className='uppercase text-white-gray text-xl mb-4'>Contact us</h3>
   			<p className='text-ice-blue text-lg font-thin'>In need of assistance? Our diligent staff is ready to help you during our business hours, no matter where you are.</p>
@@ -21,7 +21,7 @@ const Footer = () => {
       			<a className={link} href="https://www.tiktok.com/en/" target="_blank">TikTok</a>
       		</div>
       </div>
-    	<div className='flex items-center justify-center mt-10 lg:w-1/3 lg:mt-0 gap-20 lg-gap-0'>
+    	<div className='flex items-start justify-center mt-10 lg:w-1/3 lg:mt-0 gap-20 lg-gap-0'>
         <div className='w-1/2'>
     			<h3 className={h1}>Webinars</h3>
     			<a className={link} href="https://doc.rust-lang.org/rustdoc/what-is-rustdoc.html" target="_blank">Rust</a> <br/>
